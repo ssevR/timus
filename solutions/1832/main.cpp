a8ac35d5c97e6d5d2d1d33f00a293424f223a595
rvess <khaibula.2003@gmail.com>
2024-05-28 18:25:49 +0300

    1832 ac

---
+++ solutions/1832/main.cpp
@@ -0,0 +1,59 @@
+#pragma GCC optimize("O3")
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define IOS ios::sync_with_stdio(false); cin.tie(0)
+#define ll long long
+#define ld long double
+#define mp(a, b) make_pair(a, b)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pil pair<int, ll>
+#define pli pair<ll, int>
+#define fi first
+#define se second
+ 
+ 
+const int MOD = 1e9+7; // 998244353; // = (119<<23)+1
+const int MX = 2e5+5;
+const ll INF = 1e18;
+const ld PI = 4*atan((ld)1);
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+ 
+ 
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ 
+int main() {
+    int n;
+    cin >> n;
+    if (n <= 26) {
+        for (int i = 0; i < n; ++i) {
+            cout << char(i + 'a');
+        }
+        return 0;
+    }
+
+    int f = 2;
+    while(n % f == 0) {
+        ++f;
+    }
+
+    string s(n, 'a');
+    for (int i = 0; i < f; ++i) {
+        for (int j = i; j < n / 2; j += f) {
+            s[j] = char('a' + i);
+        }
+        for (int j = n / 2 + i; j < n; j += f) {
+            s[j] = char('a' + i + 13);
+        }
+    }
+    cout << s << '\n';
+}
