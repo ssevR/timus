8fa4996de64fb6a76d7888e52bf10fec0f2e2636
rvess <khaibula.2003@gmail.com>
2024-05-29 12:13:50 +0300

    1601 ac

---
+++ solutions/1601/main.cpp
@@ -0,0 +1,53 @@
+#pragma GCC optimize("O3")
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define IOS ios::sync_with_stdio(false); cin.tie(0)
+#define ll long long
+#define ld long double
+#define mp(a, b) make_pair(a, b)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pil pair<int, ll>
+#define pli pair<ll, int>
+#define fi first
+#define se second
+ 
+ 
+const int MOD = 1e9+7; // 998244353; // = (119<<23)+1
+const int MX = 2e5+5;
+const ll INF = 1e18;
+const ld PI = 4*atan((ld)1);
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+ 
+ 
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ 
+int main() {
+    string inp;
+    int flag = 1;
+    while(getline(cin, inp)) {
+        for (char c : inp) {
+            if (isalpha(c) && flag) {
+                cout << char(toupper(c));
+                flag = 0;
+            }
+            else if (c == '.' || c == '?' || c == '!') {
+                flag = 1;
+                cout << c;
+            }
+            else {
+                cout << char(tolower(c));
+            }
+        }
+        cout << '\n';
+    }
+}
